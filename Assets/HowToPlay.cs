0c3576ddcaae23b5f41ec5d41abcc8c4e2207272
Emilijus <slibinas2012@gmail.com>
2019-03-24 10:21:13 +0200

    Added how to play

---
+++ Assets/HowToPlay.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class HowToPlay : MonoBehaviour
+{
+    [SerializeField] GameObject howToPlayText;
+    
+    void Start()
+    {
+        howToPlayText.SetActive(false);
+    }
+
+    public void ShowHowToPlay()
+    {
+        howToPlayText.SetActive(true);
+    }
+}
